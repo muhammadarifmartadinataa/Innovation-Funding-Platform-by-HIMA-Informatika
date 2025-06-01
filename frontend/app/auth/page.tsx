ff5a17092c7555b9eb73fd4b8801d4f8e15bdcfb
adchandra <adechandrapratama423@gmail.com>
2025-06-02 02:12:41 +0700

    Ade Frontend : memindahkan file page.tsx  untuk login ke file auth

--- frontend/app/auth/page.tsx
+++ frontend/app/auth/page.tsx
@@ -14,7 +14,7 @@ export default function AdminLogin() {
     setError('')
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+      const res = await fetch(${process.env.NEXT_PUBLIC_API_URL}/login, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
