057ee52497b9a8fe1d5435536cb5a73e83303f92
Rismaoktaviani03 <risma_oktaviani@teknokrat.ac.id>
2025-06-05 22:23:44 +0700

    Risma: menambahkan state form di register

--- cms-innovation-funding/app/register/page.tsx
+++ cms-innovation-funding/app/register/page.tsx
@@ -4,5 +4,13 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Register() {
-  const router = useRouter();
+    const router = useRouter();
+
+    //   state form
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [occupation, setOccupation] = useState("");
+    const [role, setRole] = useState("user");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 }
