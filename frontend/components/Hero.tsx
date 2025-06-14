c50637156770ea6d89e5ab3c282eaf20c18e9adb
adchandra <adechandrapratama423@gmail.com>
2025-06-14 23:40:37 +0700

    Ade Frontend : update tampilan landing page bagian hero menambahkan logo dan nama aplikasi

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -1,21 +1,29 @@
 'use client'
 
 export default function Hero() {
-  return (
-    <section className="bg-red-800 text-white py-24 text-center px-4">
-      <h1 className="text-4xl md:text-6xl font-bold mb-6">
-        Dukung Kampanye dan Wujudkan Perubahan<br/>
-        Bersama HIMA INFORMATIKA
-      </h1>
-      <p className="text-lg md:text-xl mb-8 max-w-2xl mx-auto">
-        Bersama kita bisa membantu mewujudkan ide-ide besar melalui donasi dan dukungan nyata.
-      </p>
-      <a
-        href="/campaigns"
-        className="inline-block bg-white text-red-800 font-semibold px-6 py-3 rounded-full shadow hover:bg-gray-100 transition"
-      >
-        Lihat Campaign
-      </a>
-    </section>
-  )
+    return (
+        <section className="min-h-screen bg-gradient-to-b from-red-700 to-red-900 text-white py-24 text-center px-4 flex flex-col justify-center items-center ">
+            {/* Logo dan Judul di Kiri Atas */}
+            <div className="absolute top-6 left-6 flex items-center space-x-3">
+                <img src="/hero.png" alt="Logo HIMA" className="h-10 w-auto object-contain rounded-2xl" />
+                <span className="text-sm md:text-base font-semibold">
+                    Innovation Funding Platform by HIMA Informatika
+                </span>
+            </div>
+
+            <h3 className="text-4xl md:text-6xl font-bold mb-6">
+                Dukung Kampanye dan Wujudkan Perubahan Bersama
+                HIMA INFORMATIKA
+            </h3>
+            <p className="text-lg md:text-xl mb-8 max-w-2xl mx-auto">
+                Bersama kita bisa membantu mewujudkan ide-ide besar melalui donasi dan dukungan nyata.
+            </p>
+            <a
+                href="/campaigns"
+                className="inline-block bg-white text-red-800 font-semibold px-6 py-3 rounded-full shadow hover:bg-gray-100 transition"
+            >
+                Lihat Campaign
+            </a>
+        </section>
+    )
 }
